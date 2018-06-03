9693f609592ebaae9a277557f1ba939b2e076b29
iashchuk <221box@list.ru>
2018-06-03 04:19:25 +0300

    Облако статистики. Рефакторинг кода. Исправление замечаний

--- js/stat.js
+++ js/stat.js
@@ -1,107 +1,119 @@
 'use strict';
 
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
-var GAP_X = 50;
-var GAP_Y = 10;
-var FONT_GAP = 10;
-var TEXT_HEIGHT = 20;
-var BAR_WIDTH = 40;
-var WIN_MESSAGE = {
-  text: 'Ура вы победили!',
-  x: 120,
-  y: 40
-};
-var MESSAGE_TITLE = {
-  text: 'Список результатов:',
-  x: 120,
-  y: 60
-};
-var TEXT_FONT = '16px PT Mono';
-var MAIN_TEXT_COLOR = '#000';
-var CLOUD_COLOR = '#fff';
-var CLOUD_SHADOW = 'rgba(0, 0, 0, 0.7)';
-var SHADOW_GAP = 10;
-var HIST_HEIGHT = 150;
 
-var startBarPoint = CLOUD_X + GAP_X;
-var yourColor = 'rgba(255, 0, 0, 1)';
-
-var getColor = function () {
-  var hue = 244;
-  var saturation = Math.floor(Math.random() * 100);
-  var lightness = 50;
-
-  return 'hsl(' + hue + ',' + saturation + '%,' + lightness + '%)';
+var cloudParams = {
+  WIDTH: 420,
+  HEIGHT: 270,
+  X: 100,
+  Y: 10,
+  COLOR: '#fff',
+  SHADOW_COLOR: 'rgba(0, 0, 0, 0.7)',
+  SHADOW_GAP: 10,
+  TEXT_FONT: '16px PT Mono',
+  TEXT_COLOR: '#000'
 };
 
+var winMessageParams = {
+  TEXT: 'Ура вы победили! Список результатов:',
+  WIDTH_TEXT: 200,
+  LINE_TEXT: 20,
+  MARGIN_X: 20,
+  MARGIN_Y: 30
+};
 
+var barChart = {
+  HEIGHT: 150,
+  BAR_WIDTH: 40,
+  MARGIN_X: 50,
+  MARGIN_Y: 10,
+  LINE_HEIGHT: 20,
+  YOUR_COLOR: 'rgba(255, 0, 0, 1)',
+  HUE_COLOR: 244,
+  LIGHTNESS: 50,
+  getHueBlueColor: function () {
+    var saturation = Math.floor(Math.random() * 100);
+    return 'hsl(' + this.HUE_COLOR + ',' + saturation + '%,' + this.LIGHTNESS + '%)';
+  }
+};
+
+
+// Функция отрисовки облака
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillRect(x, y, cloudParams.WIDTH, cloudParams.HEIGHT);
 };
 
 
-var getMaxElement = function (arr) {
-  var maxElement = arr[0];
+// Функция разделения сообщения на строки
+function getWinMessage(ctx, text, maxWidth, lineHeight, marginLeft, marginTop) {
+  var words = text.split(' ');
+  var line = '';
 
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
+  for (var i = 0; i < words.length; i++) {
+    var testLine = line + words[i] + ' ';
+    var testWidth = ctx.measureText(testLine).width;
+    if (testWidth > maxWidth) {
+      ctx.fillText(line, marginLeft + cloudParams.X, marginTop + cloudParams.Y);
+      line = words[i] + ' ';
+      marginTop += lineHeight;
+    } else {
+      line = testLine;
     }
   }
 
-  return maxElement;
+  ctx.fillText(line, marginLeft + cloudParams.X, marginTop + cloudParams.Y);
+}
+
+
+// Функция поиска максимального элемента
+var getMaxElement = function (arr) {
+  return Math.max.apply(null, arr);
+};
+
+
+// Функция отрисовки столбца гистограммы
+var renderBar = function (ctx, playerPoints, playerName, maxPoints, numberOfBar) {
+
+  var x = cloudParams.X + barChart.MARGIN_X;
+  var y = cloudParams.HEIGHT - barChart.MARGIN_Y;
+  var barHeight = barChart.HEIGHT * (playerPoints / maxPoints);
+  var nextBarMargin = (barChart.BAR_WIDTH + barChart.MARGIN_X) * numberOfBar;
+
+  ctx.fillStyle = cloudParams.TEXT_COLOR;
+  ctx.fillText(Math.floor(playerPoints), x + nextBarMargin, y - barChart.MARGIN_Y - barChart.LINE_HEIGHT - barHeight);
+
+  ctx.fillStyle = barChart.getHueBlueColor();
+  if (playerName === 'Вы') {
+    ctx.fillStyle = barChart.YOUR_COLOR;
+  }
+
+  ctx.fillRect(x + nextBarMargin, y - barChart.LINE_HEIGHT - barHeight, barChart.BAR_WIDTH, barHeight);
+
+  ctx.fillStyle = cloudParams.TEXT_COLOR;
+  ctx.fillText(playerName, x + nextBarMargin, y);
 };
 
 
 window.renderStatistics = function (ctx, players, times) {
 
   // Делаем тень для облака и само облако
-  renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, CLOUD_SHADOW);
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
+  renderCloud(ctx, cloudParams.X + cloudParams.SHADOW_GAP, cloudParams.Y + cloudParams.SHADOW_GAP, cloudParams.SHADOW_COLOR);
+  renderCloud(ctx, cloudParams.X, cloudParams.Y, cloudParams.COLOR);
 
 
-  // Задаем цвет для содержимого в облаке
-  ctx.fillStyle = MAIN_TEXT_COLOR;
-
-
-  // Выводим на экран сообщения для игрока
-  ctx.font = TEXT_FONT;
-  ctx.fillText(WIN_MESSAGE.text, WIN_MESSAGE.x, WIN_MESSAGE.y);
-  ctx.fillText(MESSAGE_TITLE.text, MESSAGE_TITLE.x, MESSAGE_TITLE.y);
+  // Выводим сообщение о победе на экран
+  ctx.font = cloudParams.TEXT_FONT;
+  ctx.fillStyle = cloudParams.TEXT_COLOR;
+  getWinMessage(ctx, winMessageParams.TEXT, winMessageParams.WIDTH_TEXT, winMessageParams.LINE_TEXT, winMessageParams.MARGIN_X, winMessageParams.MARGIN_Y);
 
 
   // Получаем максимальное набранное количество очков
   var maxTime = getMaxElement(times);
 
 
-  // Выводим статистику игроков
+  // Выводим статистику игроков (гистограмму)
   for (var i = 0; i < players.length; i++) {
-
-    // Получаем высоту бара, где максимальое значение равное высоте гистограмме
-    // Остальные значения вычисляются путем умножения высоты на коэффициент (значение игрока/max значение)
-    var barPoint = HIST_HEIGHT * (times[i] / maxTime);
-
-    // Добавляем подпись ввиде полученных очков
-    ctx.fillStyle = MAIN_TEXT_COLOR;
-    ctx.fillText(Math.floor(times[i]), startBarPoint + (BAR_WIDTH + GAP_X) * i, CLOUD_HEIGHT - GAP_Y - FONT_GAP - TEXT_HEIGHT - barPoint);
-
-    // Проверяем игрока и присваеваем ему цвет бара
-    if (players[i] === 'Вы') {
-      ctx.fillStyle = yourColor;
-    } else {
-      ctx.fillStyle = getColor();
-    }
-
-    // Рисуем сам бар
-    ctx.fillRect(startBarPoint + (BAR_WIDTH + GAP_X) * i, CLOUD_HEIGHT - GAP_Y - TEXT_HEIGHT - barPoint, BAR_WIDTH, barPoint);
-
-    // Добавляем подпись в виде имени игрока
-    ctx.fillStyle = MAIN_TEXT_COLOR;
-    ctx.fillText(players[i], startBarPoint + (BAR_WIDTH + GAP_X) * i, CLOUD_HEIGHT - GAP_Y);
+    renderBar(ctx, times[i], players[i], maxTime, i);
   }
 
 
